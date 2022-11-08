fbebeb8d9fa62ab7b42eaa7ba0f94fc33bae7d26
UmeshKumari-202128 <84262718+UmeshKumari-202128@users.noreply.github.com>
2022-11-08 20:24:26 +0530

    Add files via upload

---
+++ pythonAssignment/primsAlgo.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    const int N = 5, m = 6; // N : node , m : edge
+    vector<pair<int, int>> adj[N];
+    adj[0].push_back({1, 2});
+    adj[0].push_back({3, 6});
+    adj[1].push_back({0, 2});
+    adj[1].push_back({2, 3});
+    adj[1].push_back({3, 8});
+    adj[1].push_back({4, 5});
+    adj[2].push_back({1, 3});
+    adj[2].push_back({4, 7});
+    adj[3].push_back({0, 6});
+    adj[3].push_back({1, 8});
+    adj[4].push_back({1, 5});
+    adj[4].push_back({2, 7});
+
+    int parent[N]; // to store parent of node
+
+    int key[N]; // to store cost of MST
+
+    bool mstSet[N]; // tell whether a node is part of MST or not
+
+    for (int i = 0; i < N; i++)
+        key[i] = INT_MAX, mstSet[i] = false;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    // contain the weight required to reach a node along with its index
+
+    key[0] = 0;
+    parent[0] = -1;
+    pq.push({0, 0});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        mstSet[u] = true;
+
+        for (auto it : adj[u])
+        {
+            int v = it.first;
+            int weight = it.second;
+            if (mstSet[v] == false && weight < key[v])
+            {
+                parent[v] = u;
+                key[v] = weight;
+                pq.push({key[v], v});
+            }
+        }
+    }
+
+    for (int i = 1; i < N; i++)
+        cout << parent[i] << " - " << i << " \n";
+    return 0;
+}
